2223f541a822c012bbf4b5b3ecb3d0ac0d9faf13
Valentino Copperi <copeguey@gmail.com>
2025-04-23 01:46:22 -0300

    Add TODO comment to update to new Mercado Pago API in mercadopago.ts

--- src/lib/mercadopago.ts
+++ src/lib/mercadopago.ts
@@ -1,5 +1,6 @@
 import { MercadoPagoConfig } from "mercadopago";
 
+//TODO: Cambiar a la nueva API de Mercado Pago
 export const mercadopago = new MercadoPagoConfig({
     accessToken: process.env.MP_ACCESS_TOKEN!,
 })
